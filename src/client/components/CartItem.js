b5a60c97a2b2ce550b7795aaf7981d42465559e3
tihangrai <tihangrai@gmail.com>
2020-02-09 12:32:27 -0500

    image modal for cart

--- src/client/components/CartItem.js
+++ src/client/components/CartItem.js
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
+import { useAlert } from 'react-alert';
 import { removeFromCart } from '../redux';
 
 function CartItem({ items, removeItem }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalUrl, setModalUrl] = useState('');
+  const alert = useAlert();
+
   return (
     <React.Fragment>
-      {items.map(item => (
+      {items.map((item, i) => (
         <div key={item.id} className="cart-item-component">
           <div className="cart-item-container">
-            <img src={item.imgUrl} alt="Item img" />
-            <p className="cart-item-description">{item.name}</p>
-            <p>{item.price}</p>
-            <button type="button" onClick={() => removeItem(item)}>Remove</button>
+            <div className="cart-item-img-container">
+              <div className="cart-item-img-overlay">
+                <button
+                  className="cart-item-modal-button"
+                  onClick={() => {
+                    setModalUrl(item.imgUrl);
+                    setIsOpen(true);
+                  }}
+                  type="button"
+                >
+                  <i className="fas fa-search-plus" />
+                </button>
+              </div>
+              <img className="cart-item-img" src={item.imgUrl} alt="Item img" />
+            </div>
+
+            {isOpen && <Lightbox mainSrc={modalUrl} onCloseRequest={() => setIsOpen(false)} />}
+            <div className="cart-item-description">
+              <p className="cart-item-name">{item.name}</p>
+              <p className="cart-item-color">{item.color}</p>
+            </div>
+
+            <div className="cart-item-controls">
+              <p className="cart-item-price">{`$ ${item.price}`}</p>
+            </div>
+
+            <div className="cart-item-actions">
+              <button
+                className="cart-item-remove"
+                type="button"
+                onClick={() => {
+                  removeItem(item);
+                  alert.success('Removed item');
+                }}
+              >
+                <i className="fas fa-times" />
+              </button>
+              <button className="cart-item-wishlist" type="button">
+                <i className="fas fa-heart" />
+              </button>
+            </div>
           </div>
         </div>
       ))}
@@ -26,12 +70,11 @@ CartItem.propTypes = {
 };
 
 const mapStateToProps = state => ({
-  items: state.cart.items,
+  items: state.cart.items
 });
 
 const mapDispatchToProps = dispatch => ({
   removeItem: item => dispatch(removeFromCart(item))
 });
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartItem);
