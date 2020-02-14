d808add85f263988f42b63f5e3fbb28e50960ebe
tihangrai <tihangrai@gmail.com>
2020-02-13 20:28:46 -0500

    The WAI-ARIA role attribute confers a semantic mapping to an element.

--- src/client/components/ProductCard.js
+++ src/client/components/ProductCard.js
@@ -1,5 +1,4 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
+
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Modal } from 'react-bootstrap';
@@ -14,7 +13,7 @@ function ProductCard({ product, addItem }) {
 
   return (
     <React.Fragment>
-      <div type="button" className="product-card-component" onClick={() => setShow(true)}>
+      <div role="button" tabIndex="0" className="product-card-component" onClick={() => setShow(true)} onKeyDown={() => setShow(true)}>
         <div className="product-image-container">
           <div className="product-card-overlay" />
           <img alt="Product img" src={product.imgUrl} />
